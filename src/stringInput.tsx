35595aac5d10ce8e6f74866370310b77d0b0cc88
James Conkling <james.lane.conkling@gmail.com>
2018-08-10 17:24:08 -0400

    build string input

---
+++ src/stringInput.tsx
@@ -0,0 +1,53 @@
+import React, {
+  PureComponent,
+  ChangeEvent,
+  KeyboardEvent
+} from 'react';
+
+
+type Props = {
+  value: string | null
+  className?: string
+  placeholder?: string
+  onChange: (value: string | null) => void
+  onSubmit?: (value: string | null) => void
+  onClear?: () => void
+}
+
+class StringInput extends PureComponent<Props> {
+  onChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => this.props.onChange(value === '' ? null : value)
+
+  // TODO - allow onKeyDown to be extended via incomming props,
+  // so strictly-formed is composable with withHotKeys 
+  onKeyDown(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.which === 13) {
+      // enter key
+      e.preventDefault();
+      e.stopPropagation();
+      this.props.onSubmit && this.props.onSubmit(this.props.value);
+    } else if (e.which === 27) {
+      // esc key
+      e.preventDefault();
+      e.stopPropagation();
+      this.props.onClear && this.props.onClear();
+    }
+  }
+
+  render() {
+    return (
+      <input
+        // TODO - will placeholder display w/ ''?  if not, should these take undefined | string?
+        value={this.props.value === null ? '' : this.props.value}
+        type="string"
+        className={this.props.className}
+        placeholder={this.props.placeholder}
+        tabIndex={0}
+        onKeyDown={this.onKeyDown}
+        onChange={this.onChange}
+      />
+    );
+  }
+}
+
+
+export default StringInput;
